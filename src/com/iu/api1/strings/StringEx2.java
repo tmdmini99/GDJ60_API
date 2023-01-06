06094a14d2e7974f75b23f3d2e39ec2f7d1c46f5
tmdmini99 <tmdals2451@naver.com>
2023-01-06 10:43:24 +0900

    teachers

--- src/com/iu/api1/strings/StringEx2.java
+++ src/com/iu/api1/strings/StringEx2.java
@@ -9,20 +9,21 @@ public class StringEx2 {
 		String [] names = {"jpg","jpeg","gif","png"};
 		System.out.println("파일명 입력");
 		String fileName = sc.next();
-		int check = 0;
+		boolean flag =true;
 		for(int i=0; i<names.length;i++) {
 			int idx = fileName.indexOf(".");
 			
 			if(fileName.substring(idx+1).equals(names[i])) {
-				check = 1;
+				flag =!flag;
+				System.out.println("등록 완료");
 				break;
 			}
 			
 		}
-		if(check == 1) {
-			System.out.println("등록 완료");
-		}else
+		if(flag) {
 			System.out.println("등록 실패");
+		}
+			
 		
 		
 
