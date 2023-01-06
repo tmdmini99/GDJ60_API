642f39daea06cc6f41405343e3943240941149e8
tmdmini99 <tmdals2451@naver.com>
2023-01-06 10:42:10 +0900

    change2

--- src/com/iu/api1/strings/StringEx2.java
+++ src/com/iu/api1/strings/StringEx2.java
@@ -11,12 +11,18 @@ public class StringEx2 {
 		String fileName = sc.next();
 		int idx =fileName.lastIndexOf(".");
 		String result =fileName.substring(idx+1);
+		boolean flag = true;
+		
 		for(int i=0; i<names.length; i++) {
 			if(result.equals(names[i])) {
 				System.out.println("정상");
+				flag =! flag;
 				break;
-			}else
-				System.out.println("비정상");
+			}
+				
+		}
+		if(flag) {
+			System.out.println("비정상");
 		}
 		
 	
